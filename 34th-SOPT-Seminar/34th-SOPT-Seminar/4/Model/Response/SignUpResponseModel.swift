40363f15571bcc92b56b3e34386e50af2355c276
ParkSY0919 <qhr498@gmail.com>
2024-05-03 22:49:24 +0900

    [Feat] #6 - Feat HTTP connection using Moya

---
+++ 34th-SOPT-Seminar/34th-SOPT-Seminar/4/Model/Response/SignUpResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpResponseModel.swift
+//  34th-SOPT-Seminar
+//
+//  Created by 박신영 on 4/27/24.
+//
+
+import Foundation
+
+struct SignUpResponseModel: Codable {
+    let code: Int
+    let message: String
+}
